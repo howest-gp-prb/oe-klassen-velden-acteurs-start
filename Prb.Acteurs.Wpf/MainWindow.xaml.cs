5ff8f8bccb16e762623bbcceb6253ceb86657417
jandedeuraerder-howest <jan.de.deurwaerder@howest.be>
2020-10-24 10:47:15 +0200

    Add project files.

---
+++ Prb.Acteurs.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Prb.Acteurs.Wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+        }
+        private void lstActors_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+        }
+
+        private void btnNew_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void btnEdit_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+        }
+    }
+}
